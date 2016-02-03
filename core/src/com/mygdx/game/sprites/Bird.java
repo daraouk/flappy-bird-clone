f89f2de0cdbe30e9ae26e08eb0415b4be968d567
Dara Ouk <douk1@we-bridge.net>
2016-02-03 16:11:20 +0700

    added bird animation

--- core/src/com/mygdx/game/sprites/Bird.java
+++ core/src/com/mygdx/game/sprites/Bird.java
@@ -1,6 +1,7 @@
 package com.mygdx.game.sprites;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
 
@@ -13,18 +14,22 @@ public class Bird {
 
     private Vector3 position;
     private Vector3 velocity;
-    private Texture bird;
+    private Animation birdAnimation;
+    private Texture texture;
 
     private Rectangle bounds;
 
     public Bird(int x, int y) {
         position = new Vector3(x, y, 0);
         velocity = new Vector3(0, 0, 0);
-        bird = new Texture("bird.png");
-        bounds = new Rectangle(x, y, bird.getWidth(), bird.getHeight());
+        texture = new Texture("birdanimation.png");
+        birdAnimation = new Animation(new TextureRegion(texture), 3, 0.5f); // 0.5 frame time
+        bounds = new Rectangle(x, y, texture.getWidth() / 3, texture.getHeight());
     }
 
     public void update(float dt) {
+        birdAnimation.update(dt);
+
         // only add gravity if y > 0
         if(position.y > 0) { velocity.add(0, GRAVITY, 0); }
 
@@ -45,8 +50,8 @@ public class Bird {
         return position;
     }
 
-    public Texture getTexture() {
-        return bird;
+    public TextureRegion getTexture() {
+        return birdAnimation.getFrame();
     }
 
     public void jump() {
@@ -58,6 +63,6 @@ public class Bird {
     }
 
     public void dispose() {
-        bird.dispose();
+        texture.dispose();
     }
 }
